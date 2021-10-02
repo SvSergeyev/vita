44b7e65fab633034af2293771c996e46aa82f310
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-02 16:57:35 +0300

    removed unnecessary query annotations

--- src/main/java/tech/sergeyev/vitasoft/persistence/repository/RoleRepository.java
+++ src/main/java/tech/sergeyev/vitasoft/persistence/repository/RoleRepository.java
@@ -1,8 +1,6 @@
-package tech.sergeyev.vitasoft.persistence.dao;
+package tech.sergeyev.vitasoft.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tech.sergeyev.vitasoft.persistence.model.users.Role;
 import tech.sergeyev.vitasoft.persistence.model.users.RoleNames;
@@ -11,6 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-//    @Query("SELECT r FROM Role r WHERE r.name = :name")
-    Optional<Role> findByName(/*@Param("name")*/ RoleNames name);
+    Optional<Role> findByName(RoleNames name);
 }
