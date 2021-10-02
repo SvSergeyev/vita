6bdd2330d44556bf91734111061d443f42ed63ff
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-02 16:54:49 +0300

    simplification of the main page

--- src/main/java/tech/sergeyev/vitasoft/api/MainController.java
+++ src/main/java/tech/sergeyev/vitasoft/api/MainController.java
@@ -3,29 +3,15 @@ package tech.sergeyev.vitasoft.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import tech.sergeyev.vitasoft.persistence.model.users.Person;
+import org.springframework.web.bind.annotation.RestController;
+import tech.sergeyev.vitasoft.service.PersonServiceImpl;
 
-import javax.servlet.http.HttpServletRequest;
-
-@Controller
+@RestController
 @RequestMapping("/")
 public class MainController {
-    private final PersonService personServiceImpl;
-
-    public MainController(PersonService personServiceImpl) {
-        this.personServiceImpl = personServiceImpl;
-    }
 
     @GetMapping()
-    public String index(HttpServletRequest request) {
-        Person person = personServiceImpl.getPersonByEmail(request.getUserPrincipal().getName());
-        if (request.isUserInRole("ADMIN")) {
-            return "redirect:/admins";
-        } else if (request.isUserInRole("OPERATOR")) {
-            return "redirect:/operators/" + person.getId();
-        } else if (request.isUserInRole("USER")) {
-            return "redirect:/users/" + person.getId();
-        }
-        return "redirect:/login?logout";
+    public String index() {
+        return "Main page";
     }
 }
