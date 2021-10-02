cdf0de75a0a2042724c79ca7313d1dd2e23400a8
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-02 16:52:49 +0300

    minor fixes

--- src/main/java/tech/sergeyev/vitasoft/api/auth/AuthController.java
+++ src/main/java/tech/sergeyev/vitasoft/api/auth/AuthController.java
@@ -1,6 +1,8 @@
 package tech.sergeyev.vitasoft.api.auth;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,10 +13,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import tech.sergeyev.vitasoft.payload.request.LoginRequest;
 import tech.sergeyev.vitasoft.payload.response.JwtResponse;
-import tech.sergeyev.vitasoft.persistence.dao.PersonRepository;
-import tech.sergeyev.vitasoft.persistence.dao.RoleRepository;
+import tech.sergeyev.vitasoft.persistence.repository.PersonRepository;
+import tech.sergeyev.vitasoft.persistence.repository.RoleRepository;
 import tech.sergeyev.vitasoft.security.jwt.JwtUtils;
-import tech.sergeyev.vitasoft.service.UserDetailsImpl;
+import tech.sergeyev.vitasoft.security.services.UserDetailsImpl;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -22,7 +24,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/vita/auth")
+@RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
     private final AuthenticationManager authenticationManager;
@@ -31,26 +33,24 @@ public class AuthController {
     private final PasswordEncoder encoder;
     private final JwtUtils jwtUtils;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
     @PostMapping("/signin")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsername(),
-                        loginRequest.getPassword()
-                )
-        );
+                        request.getUsername(),
+                        request.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtils.generateToken(authentication);
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         List<String> roles = userDetails.getAuthorities()
                 .stream().map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
-
         return ResponseEntity.ok(
                 new JwtResponse(jwt,
                         userDetails.getId(),
                         userDetails.getUsername(),
-                        roles)
-        );
+                        roles));
     }
 }
