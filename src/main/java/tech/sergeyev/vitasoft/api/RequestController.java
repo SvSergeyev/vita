2bf785b36ab861db3b59a1b354f1584d0742605e
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-06 22:39:47 +0300

    added TODO list

--- src/main/java/tech/sergeyev/vitasoft/api/RequestController.java
+++ src/main/java/tech/sergeyev/vitasoft/api/RequestController.java
@@ -1,49 +1,39 @@
-package tech.sergeyev.vitasoft.api.temp;
+package tech.sergeyev.vitasoft.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
-import tech.sergeyev.vitasoft.api.deprecated.RoleValidator;
 import tech.sergeyev.vitasoft.persistence.model.requests.Request;
-import tech.sergeyev.vitasoft.persistence.model.requests.Statement;
-import tech.sergeyev.vitasoft.persistence.model.users.Person;
 import tech.sergeyev.vitasoft.service.RequestService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/requests")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class RequestController extends RoleValidator {
-    private final RequestService requestService;
+public class RequestController {
+    private RequestService requestService;
     public static final Logger LOGGER = LoggerFactory.getLogger(RequestController.class);
 
-    public RequestController(RequestService requestService,
-                             PersonService personService) {
-        super(personService);
-        this.requestService = requestService;
-    }
 
+
+    //TODO: создание заявки от пользователя
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping()
     public ResponseEntity<?> create(@RequestBody Request userRequest,
                                     HttpServletRequest httpServletRequest) {
-        Person author = personService.getPersonByEmail(
-                httpServletRequest.getUserPrincipal().getName());
-        requestService.create(author, userRequest.getMessage());
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return null;
     }
 
+    //TODO: показ всех заявок пользователю или оператору
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_OPERATOR')")
     @GetMapping
     public ResponseEntity<List<Request>> show(HttpServletRequest httpServletRequest) {
-        LOGGER.info("time: " + LocalDateTime.now());
+        /*LOGGER.info("time: " + LocalDateTime.now());
         Person author = personService.getPersonByEmail(
                 httpServletRequest.getUserPrincipal().getName()
         );
@@ -62,26 +52,30 @@ public class RequestController extends RoleValidator {
                 }
             }
         }
-        return new ResponseEntity<>(unacceptedAndPersonalRequests, HttpStatus.OK);
+        return new ResponseEntity<>(unacceptedAndPersonalRequests, HttpStatus.OK);*/
+        return null;
     }
 
-
+    //TODO: показ конкретной заявки по id
     @GetMapping("/{requestId}")
     public ResponseEntity<?> read(@PathVariable(name = "requestId") int id) {
-        Request request = requestService.getRequestById(id);
+        /*Request request = requestService.getRequestById(id);
         return request != null
                 ? new ResponseEntity<>(request, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);*/
+        return null;
     }
 
+    //TODO: обновление заявки
     @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/{requestId}")
     public ResponseEntity<?> update(@PathVariable(name = "requestId") int id,
                                     @RequestBody Request request) {
-        boolean updated = requestService.update(id, request);
+        /*boolean updated = requestService.update(id, request);
         return updated
                 ? new ResponseEntity<>(HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);*/
+        return null;
     }
 
 
