8269da919c3c94ea64b845a1a97a7713bcc30490
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-06 22:38:09 +0300

    @CrossOrigin added

--- src/main/java/tech/sergeyev/vitasoft/api/MainController.java
+++ src/main/java/tech/sergeyev/vitasoft/api/MainController.java
@@ -1,12 +1,12 @@
 package tech.sergeyev.vitasoft.api;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tech.sergeyev.vitasoft.service.PersonServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/")
 public class MainController {
 
--- src/main/java/tech/sergeyev/vitasoft/api/UserController.java
+++ src/main/java/tech/sergeyev/vitasoft/api/UserController.java
@@ -7,19 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.sergeyev.vitasoft.persistence.model.requests.Request;
 import tech.sergeyev.vitasoft.persistence.model.users.Person;
 import tech.sergeyev.vitasoft.service.PersonService;
 import tech.sergeyev.vitasoft.service.RequestService;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/users")
 public class UserController {
     /*
