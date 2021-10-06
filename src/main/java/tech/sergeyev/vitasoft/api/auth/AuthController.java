79157e84146366a322542c07f915d05ca0f658fa
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-06 22:37:01 +0300

    added registration method

--- src/main/java/tech/sergeyev/vitasoft/api/auth/AuthController.java
+++ src/main/java/tech/sergeyev/vitasoft/api/auth/AuthController.java
@@ -3,6 +3,7 @@ package tech.sergeyev.vitasoft.api.auth;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,14 +13,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import tech.sergeyev.vitasoft.payload.request.LoginRequest;
+import tech.sergeyev.vitasoft.payload.request.RegistrationRequest;
 import tech.sergeyev.vitasoft.payload.response.JwtResponse;
+import tech.sergeyev.vitasoft.persistence.model.users.Person;
+import tech.sergeyev.vitasoft.persistence.model.users.Role;
+import tech.sergeyev.vitasoft.persistence.model.users.RoleNames;
 import tech.sergeyev.vitasoft.persistence.repository.PersonRepository;
 import tech.sergeyev.vitasoft.persistence.repository.RoleRepository;
 import tech.sergeyev.vitasoft.security.jwt.JwtUtils;
 import tech.sergeyev.vitasoft.security.services.UserDetailsImpl;
 
 import javax.validation.Valid;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -35,7 +42,7 @@ public class AuthController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
 
-    @PostMapping("/signin")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -52,5 +59,35 @@ public class AuthController {
                         userDetails.getId(),
                         userDetails.getUsername(),
                         roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registration(@Valid @RequestBody RegistrationRequest request) {
+        if (personRepository.existsByEmail(request.getEmail())) {
+            return new ResponseEntity<>(
+                    "Error: Email is already in use",
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+        Person user = new Person(
+                request.getName(),
+                request.getEmail(),
+                encoder.encode(request.getPassword())
+        );
+        Set<String> rolesFromRequest = request.getRoles();
+        if (rolesFromRequest != null &&
+                (rolesFromRequest.contains("ROLE_OPERATOR") || rolesFromRequest.contains("ROLE_ADMIN"))) {
+            return new ResponseEntity<>(
+                    "Error: Invalid role",
+                    HttpStatus.BAD_REQUEST);
+        } else {
+            Role role = roleRepository.findByName(RoleNames.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Role USER not found"));
+            Set<Role> roles = new HashSet<>();
+            roles.add(role);
+            user.setRoles(roles);
+        }
+        personRepository.save(user);
+        return ResponseEntity.ok("Registration completed successfully! Your login: " + user.getEmail());
     }
 }
