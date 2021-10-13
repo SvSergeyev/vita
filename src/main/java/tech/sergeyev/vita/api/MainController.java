d8b408b17802847a807bd922027f5c92966451d0
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-13 21:54:14 +0300

    interim final version

--- src/main/java/tech/sergeyev/vita/api/MainController.java
+++ src/main/java/tech/sergeyev/vita/api/MainController.java
@@ -1,17 +1,19 @@
 package tech.sergeyev.vita.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200/")
 @RequestMapping("/")
 public class MainController {
 
     @GetMapping()
-    public String index() {
-        return "Main page";
+    public ResponseEntity<?> index() {
+        return new ResponseEntity<>("Welcome to the main page, dude. It's demonstration web-service", HttpStatus.OK);
     }
 }
